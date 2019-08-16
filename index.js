4d5fc73f3aaaf0f292268f12f0ac49ffaf3ce1b5
rick427 <rchardnjoku@yahoo.com>
2019-08-16 19:27:08 +0100

    custom modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const replaceTemplate =  require('./modules/replaceTemplate');
 
 /***************************FILES SYSTEMS*****************************/
 /*Blocking Synchronous Way*/
@@ -31,22 +31,6 @@ const url = require('url');
 
 
 /*****************************SERVER**************************************/
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%COUNTRY%}/g, product.from);
-  output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if(!product.organic){
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-  return output;
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
