787443b088cb2998389e1357155603601b40ecba
rick427 <rchardnjoku@yahoo.com>
2019-08-16 15:38:13 +0100

    non blocking async way

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 
+/*Blocking Synchronous Way*/
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 //console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\n Created on ${Date.now()}`;
-console.log(textOut);
-
 //fs.writeFileSync('./txt/output.txt', textOut);
+//console.log('File written successfully')
 
-console.log('File written successfully')
+
+/*Non-Blocking asynchronous way*/
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+       console.log(data2);
+       fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+         console.log(data3);
+         fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+             console.log(`Your File has been written.`);   
+         });
+       });
+    })
+});
+console.log('Will read file');
