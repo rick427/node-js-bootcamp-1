78fda2ee5c9d69afaf9568cfa9964e10f44bdfd7
rick427 <rchardnjoku@yahoo.com>
2019-08-16 15:42:55 +0100

    adding error

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const textOut = `This is what we know about the avocado: ${textIn}.\n Created on
 
 /*Non-Blocking asynchronous way*/
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('Error');
+    
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
        console.log(data2);
        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
          console.log(data3);
          fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-             console.log(`Your File has been written.`);   
+             console.log('Your File has been written.');   
          });
        });
     })
 });
-console.log('Will read file');
+console.log('This will read file first');
